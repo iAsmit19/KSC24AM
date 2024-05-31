f9727bda3cf3415c3641f64917369b6d86e68013
Asmit <140798529+iAsmit19@users.noreply.github.com>
2024-06-01 01:05:06 +0530

    Update_Featured-0.7
    -Added the Featured Section
    -Synced the Featured Section
    -Added Animations for the Featured Section
    -Bugs Fixed (5)
    -Made the Featured Section Responsive

--- Source/Scripts/main-script.js
+++ Source/Scripts/main-script.js
@@ -22,6 +22,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Main
   // Hero Section
+  const buttonWrapper = document.querySelectorAll(".button-wrapper");
+
+  // Featured Section
+  const dotsContainer = document.querySelector(".dots-container");
+  const stars = document.querySelectorAll(".stars");
 
   // Working Bay
   const loaderTextLogic = () => {
@@ -213,6 +218,7 @@ document.addEventListener("DOMContentLoaded", () => {
       let navModelTextSpan = document.createElement("span");
       navModelTextSpan.innerHTML = element;
       navModelTextSpan.classList.add("nav-model-text-span");
+      navModelTextSpan.classList.add("nav-text-span");
       atNavModel.appendChild(navModelTextSpan);
     });
 
@@ -228,6 +234,7 @@ document.addEventListener("DOMContentLoaded", () => {
       let navStoreTextSpan = document.createElement("span");
       navStoreTextSpan.innerHTML = element;
       navStoreTextSpan.classList.add("nav-store-text-span");
+      navStoreTextSpan.classList.add("nav-text-span");
       atNavStore.appendChild(navStoreTextSpan);
     });
 
@@ -243,6 +250,7 @@ document.addEventListener("DOMContentLoaded", () => {
       let navServicesTextSpan = document.createElement("span");
       navServicesTextSpan.innerHTML = element;
       navServicesTextSpan.classList.add("nav-services-text-span");
+      navServicesTextSpan.classList.add("nav-text-span");
       atNavServices.appendChild(navServicesTextSpan);
     });
 
@@ -258,6 +266,7 @@ document.addEventListener("DOMContentLoaded", () => {
       let navAboutUsTextSpan = document.createElement("span");
       navAboutUsTextSpan.innerHTML = element;
       navAboutUsTextSpan.classList.add("nav-about-us-text-span");
+      navAboutUsTextSpan.classList.add("nav-text-span");
       atNavAboutUs.appendChild(navAboutUsTextSpan);
     });
 
@@ -306,56 +315,172 @@ document.addEventListener("DOMContentLoaded", () => {
     // heroVideoLogic();
   };
 
-  const buttonWrapper = document.querySelector(".button-wrapper");
-  const buttonMover = document.querySelector(".button-mover");
-  const buttonText = document.querySelector(".button-text");
-
   const buttonLogic = () => {
-    buttonWrapper.addEventListener("mouseenter", () => {
-      gsap.to(buttonMover, {
+    buttonWrapper.forEach((element) => {
+      element.addEventListener("mouseenter", () => {
+        const buttonMover = element.querySelectorAll(".button-mover");
+        const buttonText = element.querySelector(".button-text");
+        gsap.to(buttonMover, {
+          opacity: 1,
+        });
+
+        gsap.to(buttonText, {
+          mixBlendMode: "difference",
+          // filter: "invert(1)",
+        });
+
+        gsap.to(buttonMover, {
+          scale: 20,
+          ease: "back.in(0.1)",
+        });
+      });
+    });
+
+    buttonWrapper.forEach((element) => {
+      element.addEventListener("mouseleave", () => {
+        const buttonMover = element.querySelectorAll(".button-mover");
+        const buttonText = element.querySelector(".button-text");
+        gsap.to(buttonMover, {
+          scale: 1,
+          ease: "back.in(0.3)",
+        });
+
+        gsap.to(buttonMover, {
+          opacity: 0,
+          delay: 0.3,
+        });
+
+        gsap.to(buttonText, {
+          // filter: "none",
+          mixBlendMode: "none",
+        });
+      });
+    });
+
+    buttonWrapper.forEach((element) => {
+      element.addEventListener("mousemove", (event) => {
+        const buttonMover = element.querySelectorAll(".button-mover");
+        const buttonText = element.querySelector(".button-text");
+
+        let rect = element.getBoundingClientRect();
+        gsap.to(buttonMover, {
+          top: event.clientY - rect.top,
+          left: event.clientX - rect.left,
+        });
+      });
+    });
+  };
+
+  const headingLogic = () => {
+    const dotsCreation = () => {
+      const dotsCount = 250;
+      const dotsMinSize = 1;
+      const dotsMaxSize = 2;
+
+      for (let i = 0; i < dotsCount; i++) {
+        const dot = document.createElement("div");
+        dot.classList.add("dots");
+        dot.style.width = `${
+          Math.floor(Math.random() * (dotsMaxSize - dotsMinSize + 1)) +
+          dotsMinSize
+        }px`;
+        dot.style.height = dot.style.width;
+        dot.style.left = `${Math.random() * 100}%`;
+        dot.style.top = `${Math.random() * 100}%`;
+        dotsContainer.appendChild(dot);
+      }
+    };
+
+    document.addEventListener("mousemove", (event) => {
+      const dots = document.querySelectorAll(".dots");
+
+      dots.forEach((dot) => {
+        const rect = dot.getBoundingClientRect();
+        const scaler = Math.hypot(
+          event.clientX - rect.left - rect.width / 2,
+          event.clientY - rect.top - rect.height / 2
+        );
+
+        gsap.to(dot, {
+          duration: 0.3,
+          opacity: 1 - scaler / 300,
+        });
+      });
+    });
+
+    const dotsCreationMobile = () => {
+      const dotsCount = 70;
+      const dotsMinSize = 1;
+      const dotsMaxSize = 2;
+
+      for (let i = 0; i < dotsCount; i++) {
+        const dot = document.createElement("div");
+        dot.classList.add("dots-mobile");
+        dot.style.width = `${
+          Math.floor(Math.random() * (dotsMaxSize - dotsMinSize + 1)) +
+          dotsMinSize
+        }px`;
+        dot.style.height = dot.style.width;
+        dot.style.left = `${Math.random() * 100}%`;
+        dot.style.top = `${Math.random() * 100}%`;
+        dotsContainer.appendChild(dot);
+      }
+    };
+
+    const shootingStarsAnimation = () => {
+      let starRepeat = () => Math.floor(Math.random() * 10) + 2;
+      let starStagger = () => Math.floor(Math.random() * 10) + 1;
+
+      gsap.to(stars, {
+        height: "30px",
+        x: 1000,
+        y: 1000,
+        duration: 2,
         opacity: 1,
+        repeat: -1,
+        repeatDelay: starRepeat(),
+        stagger: starStagger(),
+        ease: "power4.inOut",
       });
+    };
 
-      gsap.to(buttonText, {
-        mixBlendMode: "difference",
-        // filter: "invert(1)",
-      });
+    dotsCreationMobile();
+    dotsCreation();
+    shootingStarsAnimation();
+  };
 
-      gsap.to(buttonMover, {
-        scale: 20,
-        ease: "back.in(0.1)",
-      });
-    });
+  const cardsHolder = document.querySelector(".cards-holder-housing");
+  const scrollSpeed = 1.2;
 
-    buttonWrapper.addEventListener("mouseleave", () => {
-      gsap.to(buttonMover, {
-        scale: 1,
-        ease: "back.in(0.3)",
-      });
+  const featuredSection = () => {
+    // ScrollTrigger.create({
+    //   start: "top top",
+    //   end: "bottom bottom",
+    //   scrub: 5,
+    //   onUpdate: () => {
+    //     console.log("wassup");
+    //     gsap.to(cardsHolder, {
+    //       x: -window.scrollY * scrollSpeed,
+    //       ease: "linear",
+    //       overwrite: "auto",
+    //     });
+    //   },
+    // });
 
-      gsap.to(buttonMover, {
-        opacity: 0,
-        delay: 0.3,
-      });
-
-      gsap.to(buttonText, {
-        // filter: "none",
-        mixBlendMode: "none",
-      });
-    });
-
-    buttonWrapper.addEventListener("mousemove", (event) => {
-      let rect = buttonWrapper.getBoundingClientRect();
-      gsap.to(buttonMover, {
-        top: event.clientY - rect.top,
-        left: event.clientX - rect.left,
-      });
+    Draggable.create(cardsHolder, {
+      type: "x",
+      ease: "linear",
+      edgeResistance: 0.3,
+      bounds: ".cards-holder",
+      zIndexBoost: false,
     });
   };
 
   const theMain = () => {
     heroSection();
     buttonLogic();
+    headingLogic();
+    featuredSection();
   };
 
   const EXECUTIONOR = () => {
